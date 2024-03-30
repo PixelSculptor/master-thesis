2a3e12415bf1027e89e7def7864397f5bddbd263
Kacper Nadstoga <kapix007@gmail.com>
2024-03-30 22:33:58 +0100

    create lambda and attatch general permissions

--- serverless-design-patterns/packages/functions/src/simpleComputing.ts
+++ serverless-design-patterns/packages/functions/src/simpleComputing.ts
@@ -1,8 +1,31 @@
+import { S3 } from 'aws-sdk';
 import handler from '../../core/src/handler';
+import { MovieType } from '../../types/MovieType';
+import { mostFamousMovies } from '../../core/src/computing/popularityOfMovies';
+const s3 = new S3();
 
 export const main = handler(async (event) => {
     // Get Data from S3 in loop
     // For each file in the bucket perform set of tasks
     // After computing data on each file count up counter in DynamoDB
+    const fileNames = ['A'] as const;
+    const bucketName =
+        'kacper-serverless-design--moviedatasetbuckete8282f-s4k9patlpqo6';
+
+    try {
+        const data = await s3
+            .getObject({
+                Bucket: bucketName,
+                Key: `data/${fileNames[0]}.json`
+            })
+            .promise();
+        if (data.Body === undefined) throw new Error('No data in file');
+        const movies: MovieType[] = JSON.parse(data.Body.toString());
+        const mostFamousMetric = mostFamousMovies(movies);
+        console.log(mostFamousMetric[0]);
+    } catch (error) {
+        if (error instanceof Error) console.log(error.message);
+    }
+
     return JSON.stringify({ message: 'Hello from simple computing!' });
 });
--- serverless-design-patterns/stacks/Storage.stack.ts
+++ serverless-design-patterns/stacks/Storage.stack.ts
@@ -1,4 +1,5 @@
-import { Bucket, StackContext, Table } from 'sst/constructs';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { Bucket, Function, StackContext, Table } from 'sst/constructs';
 
 export function StorageStack({ stack }: StackContext) {
     const table = new Table(stack, 'FinishExecution', {
@@ -12,8 +13,45 @@ export function StorageStack({ stack }: StackContext) {
 
     const resourceBucket = new Bucket(stack, 'MovieDatasetBucket');
 
+    const s3ManipulationRole = new iam.Role(
+        stack,
+        'LambdaS3ManipulationRole2',
+        {
+            assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+            managedPolicies: [
+                iam.ManagedPolicy.fromAwsManagedPolicyName(
+                    'service-role/AmazonS3ObjectLambdaExecutionRolePolicy'
+                )
+            ]
+        }
+    );
+
+    s3ManipulationRole.addToPolicy(
+        new iam.PolicyStatement({
+            actions: ['s3:*'],
+            resources: [`${resourceBucket.bucketArn}/*`]
+        })
+    );
+
+    const simpleComputing = new Function(stack, 'SimpleComputing', {
+        handler: 'packages/functions/src/simpleComputing.main',
+        role: s3ManipulationRole
+    });
+
+    simpleComputing.attachPermissions([resourceBucket]);
+
+    // TODO: doesn't work
+    // simpleComputing.attachPermissions([
+    //     new iam.PolicyStatement({
+    //         actions: ['s3:GetObject', 's3:PutObject'],
+    //         effect: iam.Effect.ALLOW,
+    //         resources: [`${resourceBucket.bucketArn}/*`]
+    //     })
+    // ]);
+
     return {
         table,
-        resourceBucket
+        resourceBucket,
+        simpleComputing
     };
 }
