e19facbe800655b62bc63005d6fbe86a8a3e229a
Kacper Nadstoga <kapix007@gmail.com>
2024-04-03 22:34:34 +0200

    update .gitignore and create script for runnin lambdas 10 times

---
+++ serverless-design-patterns/scripts/runPatternProcessing.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# env vars
+source .env
+
+pattern=$1
+
+# function to start processing on AWS
+runProcessing(){
+    local patternName=$1
+    echo "Running processing for $patternName"
+    local API_ENDPOINT="$API_URL/$patternName"
+    for i in {1..10}
+    do
+        echo "Request $i"
+        curl -X GET $API_ENDPOINT
+    done
+}
+
+
+case $pattern in
+    "simpleComputing")
+        runProcessing "simpleComputing"
+        ;;
+    *)
+        echo "Invalid pattern name"
+        ;;
+esac
+
+
